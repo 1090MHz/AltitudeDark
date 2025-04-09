8d2df6d4b39d590ef49ab223abe1b59d0166d14d
1090MHz <107646764+1090MHz@users.noreply.github.com>
2025-04-08 19:30:21 -0600

    extend colors + fontFamily and add safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   content: ["./layouts/**/*.html", "./content/**/*.md", "./static/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "menu-bg": "#23282e", // Menu background color
+        "menu-hover": "#1678b5", // Hover background color
+        "menu-text": "#f8fafb", // Menu text color
+        "link-blue": "#3794ff", // Hyperlink color
+        "body-bg": "#000000", // Match X-Plane 12 background color
+        "body-text": "#f8fafb", // Match X-Plane 12 text color
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"], // Roboto font for menu items
+        comic: ['"Comic Neue"', "Comic Sans MS", "Comic Sans", "cursive"], // Comic Neue for body
+      },
+    },
   },
+  safelist: [
+    "group-hover/menu-depth-0:block",
+    "group-hover/menu-depth-1:block",
+    "group-hover/menu-depth-2:block",
+    "group-hover/menu-depth-3:block",
+    "group-hover/menu-depth-4:block",
+    // Although it is possible to use more than 4 levels of menu-depth, it is not recommended
+  ],
   plugins: [],
 };
