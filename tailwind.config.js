2a4e8395cb0aa152daba241639018176327c4734
1090MHz <107646764+1090MHz@users.noreply.github.com>
2025-04-15 17:22:58 -0600

    code clean-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,30 +5,9 @@
 module.exports = {
   content: ["./layouts/**/*.html", "./content/**/*.md", "./static/**/*.js"],
   theme: {
-    extend: {
-      colors: {
-        "menu-bg": "#23282e", // Menu background color
-        "menu-hover": "#1678b5", // Hover background color
-        "menu-text": "#f8fafb", // Menu text color
-        "link-blue": "#3794ff", // Hyperlink color
-        "body-bg": "#000000", // Match X-Plane 12 background color
-        "body-text": "#f8fafb", // Match X-Plane 12 text color
-      },
-      fontFamily: {
-        roboto: ["Roboto", "sans-serif"], // Roboto font for menu items
-        comic: ["Comic Neue", "Comic Sans MS", "Comic Sans", "cursive"], // Comic Neue for body
-      },
-    },
+    extend: {},
   },
   safelist: [
-    "font-roboto",
-    "font-comic",
-    "menu-bg", // Menu background color
-    "menu-hover", // Hover background color
-    "menu-text", // Menu text color
-    "link-blue", // Hyperlink color
-    "body-bg", // Match X-Plane 12 background color
-    "body-text", // Match X-Plane 12 text color
     "group-hover/menu-depth-0:block",
     "group-hover/menu-depth-1:block",
     "group-hover/menu-depth-2:block",
@@ -36,5 +15,5 @@ module.exports = {
     "group-hover/menu-depth-4:block",
     // Although it is possible to use more than 4 levels of menu-depth, it is not recommended
   ],
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/typography")],
 };
