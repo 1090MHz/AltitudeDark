ff5b0efc42d71aa5ea5b912da358eae473d32f1f
1090MHz <107646764+1090MHz@users.noreply.github.com>
2025-04-22 18:52:42 -0600

    Formatting + ignore function-url-quotes

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-    extends: [
-      'stylelint-config-standard',
-      'stylelint-config-tailwindcss',
-    ],
-    rules: {
-      // Allow Tailwind's @apply
-      'at-rule-no-deprecated': null,
-  
-      // Enforce long hex codes like #ffffff instead of #fff
-      'color-hex-length': 'long',
-    },
-  };
-  
+  extends: ["stylelint-config-standard", "stylelint-config-tailwindcss"],
+  rules: {
+    // Disable adding function-url-quotes
+    "function-url-quotes": null,
+
+    // Allow Tailwind's @apply
+    "at-rule-no-deprecated": null,
+
+    // Enforce long hex codes like #ffffff instead of #fff
+    "color-hex-length": "long",
+  },
+};
