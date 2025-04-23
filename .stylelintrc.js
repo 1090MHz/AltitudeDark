9816f9b68b4f5cd947fb92dc9a2186728b0b7040
1090MHz <107646764+1090MHz@users.noreply.github.com>
2025-04-22 18:19:47 -0600

    use  stylelintrc.js instead

---
+++ .stylelintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [
+      'stylelint-config-standard',
+      'stylelint-config-tailwindcss',
+    ],
+    rules: {
+      // Allow Tailwind's @apply
+      'at-rule-no-deprecated': null,
+  
+      // Enforce long hex codes like #ffffff instead of #fff
+      'color-hex-length': 'long',
+    },
+  };
+  
